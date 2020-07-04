3fc1eac83d8609931f0f32758561e1d6cc408810
adithep <adtp2221@gmail.com>
2020-07-03 23:50:59 -0700

    will fix timer.invalidate() fix and will add cancel button

--- Egg Timers/ViewController.swift
+++ Egg Timers/ViewController.swift
@@ -7,14 +7,73 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var timer = Timer()
+    var player: AVAudioPlayer?
+    var secondsPassed = 0
+    var totalTime = 0
+    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 7, "Hard":9]
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var showLabel: UILabel!
+    @IBOutlet weak var showTimerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func eggButtonPressed(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        
+        timer.invalidate()
+        totalTime = eggTimes[hardness]!
+        showLabel.text = hardness
+        progressBar.progress = 0.0
+        
+        timerFlow(totalTime)
+    }
+    
+    //MARK: - Sound
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
+    
+    //MARK: - Slider Bar
+    @objc func updateBar(_ totalTime: Int) {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
+        } else {
+            timer.invalidate()
+            self.showTimerLabel.text = "DONE"
+            self.playSound()
+        }
+    }
+    
+    //MARK: - Time
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
     }
 
-
+    func timerFlow(_ seconds: Int) {
+        var convertedSecond = seconds
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (updateBar) in
+            if convertedSecond > 0 {
+                self.showTimerLabel.text = self.timeFormatted(convertedSecond)
+                convertedSecond -= 1
+                self.updateBar(convertedSecond)
+            }
+        }
+    }
 }
 
