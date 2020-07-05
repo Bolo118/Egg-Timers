828e7486c855b0dd6d18b6a2fdcba5ccfdba26ca
adithep <adtp2221@gmail.com>
2020-07-05 12:53:20 -0700

    timer.invalidate fixed

--- Egg Timers/ViewController.swift
+++ Egg Timers/ViewController.swift
@@ -15,27 +15,25 @@ class ViewController: UIViewController {
     var player: AVAudioPlayer?
     var secondsPassed = 0
     var totalTime = 0
-    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 7, "Hard":9]
+    var aTime = 0
+    let eggTimes: [String: Int] = ["Soft": 300, "Medium": 420, "Hard":540]
     
     @IBOutlet weak var progressBar: UIProgressView!
     
     @IBOutlet weak var showLabel: UILabel!
     @IBOutlet weak var showTimerLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
     
     @IBAction func eggButtonPressed(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         
         timer.invalidate()
-        totalTime = eggTimes[hardness]!
         showLabel.text = hardness
-        progressBar.progress = 0.0
-        
-        timerFlow(totalTime)
+        totalTime = eggTimes[hardness]!
+        aTime = eggTimes[hardness]!
+        secondsPassed = 0
+    
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateBar), userInfo: nil, repeats: true)
     }
     
     //MARK: - Sound
@@ -45,12 +43,16 @@ class ViewController: UIViewController {
         player?.play()
     }
     
-    //MARK: - Slider Bar
-    @objc func updateBar(_ totalTime: Int) {
+    //MARK: - Progress Bar
+    @objc func updateBar() {
         if secondsPassed < totalTime {
             secondsPassed += 1
             let percentageProgress = Float(secondsPassed) / Float(totalTime)
             progressBar.progress = percentageProgress
+            if aTime > 0 {
+                self.showTimerLabel.text = self.timeFormatted(self.aTime)
+                aTime -= 1
+            }
         } else {
             timer.invalidate()
             self.showTimerLabel.text = "DONE"
@@ -65,15 +67,16 @@ class ViewController: UIViewController {
         return String(format: "%02d:%02d", minutes, seconds)
     }
 
-    func timerFlow(_ seconds: Int) {
-        var convertedSecond = seconds
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (updateBar) in
-            if convertedSecond > 0 {
-                self.showTimerLabel.text = self.timeFormatted(convertedSecond)
-                convertedSecond -= 1
-                self.updateBar(convertedSecond)
-            }
-        }
-    }
+//    func timerFlow(_ seconds: Int) {
+//        var convertedSecond = seconds
+//        timer.invalidate()
+//        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (updateBar) in
+//            if convertedSecond > 0 {
+//                self.showTimerLabel.text = self.timeFormatted(convertedSecond)
+//                convertedSecond -= 1
+//                self.updateBar(convertedSecond)
+//            }
+//        }
+//    }
 }
 
